b2e43dd7225fc30a6b74077dfb9bc6a570605e26
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2016-08-15 20:28:25 +0300

    reaction page update

--- spec/ReactionSpec.js
+++ spec/ReactionSpec.js
@@ -1,9 +1,16 @@
 describe("Reactions", function() {
 
     var urlRelation = "http://metabolitics.biodb.sehir.edu.tr/api3/reaction/PTAr";
-    it("should be response metabolite detail (" + urlRelation + ")", function(done) {
+    it("should be response reaction detail (" + urlRelation + ")", function(done) {
         $.get(urlRelation, function(data) {
             expect(data.id).toEqual("PTAr");
+            done();
+        });
+    });
+
+    var urlRelatedRelation = "http://metabolitics.biodb.sehir.edu.tr/api3/relatedmetabolites/PTAr";
+    it("should be response related metabolites detail (" + urlRelatedRelation + ")", function(done) {
+        $.get(urlRelation, function(data) {
             expect(data.metabolites.length > 0).toBeTruthy();
             done();
         });
