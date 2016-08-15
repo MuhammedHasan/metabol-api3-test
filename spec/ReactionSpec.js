3db575fcc6949fea1043241809c52c76f2579823
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2016-08-15 20:31:46 +0300

    bug dix

--- spec/ReactionSpec.js
+++ spec/ReactionSpec.js
@@ -10,7 +10,7 @@ describe("Reactions", function() {
 
     var urlRelatedRelation = "http://metabolitics.biodb.sehir.edu.tr/api3/relatedmetabolites/PTAr";
     it("should be response related metabolites detail (" + urlRelatedRelation + ")", function(done) {
-        $.get(urlRelation, function(data) {
+        $.get(urlRelatedRelation, function(data) {
             expect(data.metabolites.length > 0).toBeTruthy();
             done();
         });
